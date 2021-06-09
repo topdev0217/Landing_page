177b7c28ddb4648590f4bd5fed5be03119e89b75
Min <dev_min@nwg.gr>
2021-06-10 05:20:28 +0800

    Feat: header component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' render={() => <Home />} />
+          <Redirect to='/' />
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import {
+  Link,
+  useHistory
+} from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import dollarImg from '../../assets/icons/dollar.svg';
+import boxingImg from '../../assets/icons/boxing.svg';
 import './style.scss';
 
 const Header = () => {
+  const history = useHistory();
+
+  /**
+   * Go to url by a click
+   * @param {String} url
+   */
+  const linkTo = (url) => {
+    history.push(url)
+  }
+
   return (
     <div className='header'>
-      <Container style={{ height: '70px' }}>
-        <Typography component="div" style={{color: 'white'}}>
-          Header Content
+      <Container className='container'>
+        <Typography component='div' className='logo-section'>
+          <Typography component='div' className='icon-list' onClick={() => linkTo('/')}>
+            <img src={dollarImg} aria-hidden alt='dollar image' />
+            <img src={boxingImg} aria-hidden alt='boxing image' />
+          </Typography>
+          <Typography component='div' className='divider' />
+        </Typography>
+        <Typography component='div' className='menu-list'>
+          <Link to='/' className={window.location.pathname === '/' ? 'active' : 'item'}>Marketplace</Link>
+          <Link to='/about' className={window.location.pathname === '/about' ? 'active' : 'item'}>About</Link>
+          <Button variant='contained'>Sign up for the waiting list</Button>
         </Typography>
       </Container>
     </div>
